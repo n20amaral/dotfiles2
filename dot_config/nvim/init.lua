29ea837dde6e18cc75efdbda5f3df7c1586154c5
n20amaral <n20amaral@outlook.com>
2026-02-13 10:21:43 -0100

    fixed tokyonight url

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,15 +1,4 @@
 vim.pack.add({
-  'folke/tokyonight',
+  'folke/tokyonight.nvim',
   'catppuccin/nvim'
 })
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.schedule(vim.cmd.pack.update)
-  end,
-})
-
-require('catppuccin').setup({ flavour = 'mocha' })
-
-vim.api.nvim_create_user_command('Tokyo', 'colorscheme tokyonight', {})
-vim.api.nvim_create_user_command('Cat', 'colorscheme catppuccin', {})
